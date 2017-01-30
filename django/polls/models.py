02a91d8df3b7806ae8e0c47cfd9855c6c04b5ceb
John yang <johnyang@ML000731184.lan>
2017-01-30 16:22:36 +0800

    django

---
+++ django/polls/models.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+import datetime
+
+# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    def was_published_recentliy(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    Choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.Choice_text
+
---
+++ django/polls/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
+from django.template import loader
+# Create your views here.
+
+#def index(request):
+#    return HttpResponse('Hello,world. You are at the polls index')
+
+#def index(request):
+#    last_question_list = Question.objects.order_by('-pub_date')[:5]
+#    output = ','.join([q.question_text for q in last_question_list])
+#    return HttpResponse(output)
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question  %s." % question_id)
+
